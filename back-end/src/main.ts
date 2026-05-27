40c7f9f9f5b85a2597fc80e2e06ac87426e493fc
Mairon Lima <maironlmelo@gmail.com>
2026-05-27 20:54:59 -0300

    teste 0.8

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://hosted-images.vercel.app/',
+    origin: 'hosted-images.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
